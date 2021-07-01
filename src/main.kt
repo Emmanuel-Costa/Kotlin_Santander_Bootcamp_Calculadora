688fcf1df1185576230425d36c367b547447c05c
Emmanuel-Costa <emmanuelcosta@id.uff.br>
2021-07-01 11:18:54 -0300

    Improving the code

--- src/main.kt
+++ src/main.kt
@@ -2,19 +2,18 @@ import kotlin.math.pow
 
 fun main() {
     val y: Float? = 55.0F
-    val i: Float = y ?: 0F
     val z: Float? = 78.0F
-    val x: Float = z ?: 0F
-    if (i != 0F && x != 0F) {
+
+    if (y != null && z != null) {
         val operator = 5
         when (operator) {
-            0 -> println(x + i)             // adição
-            1 -> println(x - i)             // subtração
-            2 -> println(x * i)             // multiplicação
-            3 -> println(x / i)             // divisão
-            4 -> println(x % i)             // resto da divisão
-            5 -> println(x.pow(i))          // exponenciação
-            else -> println((x / 100) * i)  // porcentagem
+            0 -> println(y + z)             // adição
+            1 -> println(y - z)             // subtração
+            2 -> println(y * z)             // multiplicação
+            3 -> println(y / z)             // divisão
+            4 -> println(y % z)             // resto da divisão
+            5 -> println(y.pow(z))          // exponenciação
+            else -> println((y / 100) * z)  // porcentagem
         }
 
     } else {
